3c8a388a54fef1767ec0eaa2c23bece180f3fedd
celinecattin <celine.cattin@stud.kswe.ch>
2024-04-28 20:16:43 +0200

    -Das Programm gibt den Bauplan (sprich die Koordinaten für die Perlen) des Perlenmobiles an. -Die Anzahl Perlen ist jedoch noch nicht optimiert.

--- testing1.py
+++ testing1.py
@@ -1,58 +1,52 @@
-import numpy as np
-from mnist import MNIST
+import numpy as np #importing the numpy library
+from mnist import MNIST #importing the mnist library
 
 
+# training set with letters
 mnist = MNIST()
+# mnist.train_set.images  (60000, 784) (60000 images, 784 values for the 28*28 grid; arranged as array)
+# mnist.train_set.labels  (60000, 10) (60000 images, 10 different possible labels -one hot encoded (for deep learning))
 
-# Train set is lazily loaded into memory and cached afterward
-mnist.train_set.images  # (60000, 784)
-mnist.train_set.labels  # (60000, 10)
+num1 = 4
+img1 = mnist.train_set.images[num1,:].reshape(28,28) # Take image num1+1 of the training set and reshape its array to a 28 * 28 grid
+img1 = np.rot90 (img1,-1,(0,1)) # rotate the image
+a = mnist.train_set.labels[num1,:] # assign the corresponding label to a
+vol1 = np.tile(img1,(28,1,1)) # img1 is stacked on top of each other 28 times
+print(np.where(a == 1)[0][0]) # printing the position of label a which corresponds to the number in the img1
+# [0][0] converts it to an integer
 
-# Test set is lazily loaded into memory and cached afterward
-mnist.test_set.images   # (10000, 784)
-mnist.test_set.labels   # (10000, 10)
+num2 = 9
+img2 = mnist.train_set.images[num2,:].reshape(28,28) # Take image num2+1 of training set and reshape its array to a 28*28 grid
+img2 = np.rot90 (img2,-1,(0,1)) # rotate image
+b = (mnist.train_set.labels[num2,:]) # assign the corresponding label to b
+vol2 = np.tile(img2,(28,1,1)) # img2 ist stacked on top of each other 28 times
+vol2 = np.rot90(vol2,1) # rotate vol2 to show the number from another side as vol1
+print(np.where(b ==1)[0][0]) # printing the position of label b which corresponds to the number of img2
 
-# Yield minibatches from the shuffled train set
-#for images, labels in mnist.train_set.minibatches(batch_size=256):
-#    pass
+cube = vol1 * vol2 # multiplying the two cubes of the two images
+cubegraph = cube > 0 # print only the points where there is a number > 1 (because 0 = no color, 1= black )
+cube[cubegraph] = 1. # converting all values that are true in cubegraph to a 1.
 
-img = mnist.train_set.images[4,:].reshape(28,28)
-img = np.rot90 (img,-1,(0,1))
-a = mnist.train_set.labels[0,:]
-vol1 = np.tile(img,(28,1,1))
-print(np.where(a ==1)[0][0])
+print(cubegraph)
 
-img1 = mnist.train_set.images[9,:].reshape(28,28)
-img1 = np.rot90 (img1,-1,(0,1))
-b = (mnist.train_set.labels[2,:])
-vol2 = np.tile(img1,(28,1,1))
-vol2 = np.rot90(vol2,1)
-print(np.where(b ==1)[0][0])
-
-cube = vol1 * vol2
-cubegraph = cube > 0
-print(cube)
-
-anzahlperlen=np.size(np.where(cubegraph==True))
+anzahlperlen=np.size(np.where(cubegraph==True)) # count the printed points
 print(anzahlperlen)
 
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt # import libraries to display the cube
 
-# Generate some random 3D numpy array
-array_3d = cubegraph
 
 # Create a meshgrid for x, y, z coordinates
-x, y, z = np.meshgrid(np.arange(array_3d.shape[0]),
-                      np.arange(array_3d.shape[1]),
-                      np.arange(array_3d.shape[2]))
+x, y, z = np.meshgrid(np.arange(cubegraph.shape[0]), # make a meshgrid 28*28 (the shape of cubegraph of the x axes(x=0))
+                      np.arange(cubegraph.shape[1]),
+                      np.arange(cubegraph.shape[2]))
 
 # Create the 3D plot
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+ax = fig.add_subplot(111, projection='3d') # put the plot in the field 111 (because I have only 1 plot)
 
 # Plot the points
-ax.scatter(x[cubegraph], y[cubegraph], z[cubegraph], c=array_3d[cubegraph].flatten(), cmap='viridis')
+ax.scatter(x[cubegraph], y[cubegraph], z[cubegraph]) # plot the points where cubegraph is true
+
 
 # Add labels
 ax.set_xlabel('X')
@@ -61,3 +55,15 @@ ax.set_zlabel('Z')
 
 # Show the plot
 plt.show()
+
+# definition to make a construction plan for my model with pearls
+def printBauplan(cubegraph):
+    g = np.arange(cubegraph.shape[2]) # make an array the length of the z axes
+
+    for x in range(cubegraph.shape[0]): # check all combinations of x and y coordinates in cubegraph
+        for y in range(cubegraph.shape[1]):
+            v = g[cubegraph[x,y,:]] # g assigns a number from the array to each true value on the z axes (v) at point x,y
+            if len(v) > 0: # if there are true values, it prints them.
+                print(x,y,v)
+
+printBauplan(cubegraph)
