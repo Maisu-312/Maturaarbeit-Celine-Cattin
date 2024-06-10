52349a144d7b7e9c2ce7e14e7825e55e1032392d
celinecattin <celine.cattin@stud.kswe.ch>
2024-06-10 21:42:18 +0200

    - Mit der Definition perlenoptimieren wird die Anzahl Punkte reduziert.

--- testing1.py
+++ testing1.py
@@ -1,20 +1,16 @@
-import numpy as np  #importing the numpy library
-from mnist import MNIST  #importing the mnist library
+import numpy as np  # importing the numpy library
+from mnist import MNIST  # importing the mnist library
 
 from PIL import Image
 
-def photoshopimage(filepath): #definition for importing drawings from photoshop
+# definition for importing drawings from photoshop:
+def photoshopimage(filepath): # Achtung! Beim Filepath \\ einfügen!
     #
-    pic = Image.open(filepath) #open the image
-    pix = np.array(pic)#convert into an np. array
+    pic = Image.open(filepath) # open the image
+    pix = np.array(pic)# convert into an np. array
     pix = pix[:,:,0] < 32 # choose one layer of values and only keep the pixels with values lower than 32 (the black values)
-    return(pix)
+    return(1*pix) # 1 * pix to convert the true and false values into 1 and 0
 
-
-# training set with letters
-mnist = MNIST()
-# mnist.train_set.images  (60000, 784) (60000 images, 784 values for the 28*28 grid; arranged as array)
-# mnist.train_set.labels  (60000, 10) (60000 images, 10 different possible labels -one hot encoded (for deep learning))
 
 def mnistimage(num): # definition to import arrays of mnist
     img = mnist.train_set.images[num, :].reshape(28,28)  # Take image num1+1 of the training set and reshape its array to a 28 * 28 grid
@@ -22,6 +18,25 @@ def mnistimage(num): # definition to import arrays of mnist
     print(np.where(a == 1)[0][0])  # printing the position of label a which corresponds to the number in the img1
     # [0][0] converts it to an integer
     return(img)
+
+def perlenoptimieren(cube): # definition to minimize the number of points plotted
+    sz = cube.shape
+    for z in range(sz[2]): # für alle Ebenen in die z Richtung
+        for x in range(sz[0]): # für alle Zeilen in die x Richtung.
+            for y in range(sz[1]): # für alle Spalten in die y Richtung.
+                if cube[x, y, z] == 1:
+                    row = cube[x, :, z]
+                    column = cube[:, y, z]
+                    if sum(row) > 1 and sum(column) > 1: # Wenn in der gleichen Reihe und Spalte auch bereits eine 1 ist,
+                        cube[x, y, z] = 0 # Dann wird die 1 zu einer 0 umgewandelt.
+    return(cube)
+
+
+# training set with letters
+mnist = MNIST()
+# mnist.train_set.images  (60000, 784) (60000 images, 784 values for the 28*28 grid; arranged as array)
+# mnist.train_set.labels  (60000, 10) (60000 images, 10 different possible labels -one hot encoded (for deep learning))
+
 
 
 img1 = photoshopimage("C:\\Users\\Celine\\Documents\\Kanti Jahr 3\\Maturaarbeit\\png files\\note1.png")
@@ -37,21 +52,16 @@ vol1 = np.tile(img1, (28, 1, 1))  # img1 is stacked on top of each other 28 time
 img2 = np.rot90(img2, -1, (0, 1))  # rotate image
 vol2 = np.tile(img2, (28, 1, 1))  # img2 ist stacked on top of each other 28 times
 vol2 = np.rot90(vol2, 1)  # rotate vol2 to show the number from another side as vol1
-#print(np.where(b == 1)[0][0])  # printing the position of label b which corresponds to the number of img2
+# print(np.where(b == 1)[0][0])  # printing the position of label b which corresponds to the number of img2
 
 cube = vol1 * vol2  # multiplying the two cubes of the two images
 cubegraph = cube > 0  # print only the points where there is a number > 1 (because 0 = no color, 1= black )
 cube[cubegraph] = 1.  # converting all values that are true in cubegraph to a 1.
 
-# def perlenoptimieren (cubegraph):
-#     x = 28 #x ist gleich Länge der cube Seite
-#     for [x][:] in range(x): # für jede einzelne Ebene , also for z Achse in range
-#         for # jeden einzelnen Punkt beginnend bei...
-#             if np.where(==1) and np.where(==1) # wenn es in der selben Reihe und Kolume auch eine 1 hat
-#             1 = 0 # dann das 1 in eine 0 umwandeln.
+cube = perlenoptimieren(cube)
+cubegraph = cube > 0  # print only the points where there is a number > 1 (because 0 = no color, 1= black )
 
 
-print(cubegraph)
 
 anzahlperlen = np.size(np.where(cubegraph == True))  # count the printed points
 print(anzahlperlen)
