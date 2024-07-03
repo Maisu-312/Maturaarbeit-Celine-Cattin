16e4b0f04ad080db56188191334ae4f00657c7c9
celinecattin <celine.cattin@stud.kswe.ch>
2024-07-03 22:18:59 +0200

    - Mit der Definition calculatemin wird die minimale mögliche Anzahl Punkte für ein Modell ausgerechnet. -Die Funktion CalcMinCube sollte berechnen, wie diese Punkte verteilt werden müssen. Die erste Version funktioniert jedoch noch nicht ganz;)

--- testing1.py
+++ testing1.py
@@ -6,7 +6,7 @@ from PIL import Image
 # definition for importing drawings from photoshop:
 def photoshopimage(filepath): # Achtung! Beim Filepath \\ einfügen!
     pic = Image.open(filepath) # open the image
-    pix = np.array(pic)# convert into an np. array
+    pix = np.array(pic) # convert into an np. array
     pix = pix[:,:,0] < 32 # choose one layer of values and only keep the pixels with values lower than 32 (the black values)
     return(1*pix) # 1 * pix to convert the true and false values into 1 and 0
 
@@ -30,6 +30,16 @@ def perlenoptimieren(cube): # definition to minimize the number of points plotte
                         cube[x, y, z] = 0 # Dann wird die 1 zu einer 0 umgewandelt.
     return(cube)
 
+# definition to make a construction plan for my model with pearls
+def printBauplan(cubegraph):
+    g = np.arange(cubegraph.shape[2])  # make an array the length of the z axes
+
+    for x in range(cubegraph.shape[0]):  # check all combinations of x and y coordinates in cubegraph
+        for y in range(cubegraph.shape[1]):
+            v = g[cubegraph[x, y,:]]  # g assigns a number from the array to each true value on the z axes (v) at point x,y
+            if len(v) > 0:  # if there are true values, it prints the z coordinate of these values. (seen from bottom to top)
+                print(x, y, v)
+
 
 def optimizeAllSides(cube): # optimize the number of points starting from the 4 different sides of the cube
     cube1 = perlenoptimieren(cube)
@@ -50,8 +60,99 @@ def optimizeAllSides(cube): # optimize the number of points starting from the 4 
     return(cube1,cube2,cube3,cube4)
 
 
+def calculatemin (cube): # calculating the minimal number of points necessary to display this model
+    print("Die Minimums der Ebenen sind:")
+    sz = cube.shape
+    summe = 0
+    for z in range(sz[2]): # für jede Ebene
+        a = np.sum(cube[:,:,z],0) # Die Summe der Punkte in allen Reihen in x - Richtung zusammenzählen.
+        b = np.sum(cube[:,:,z],1) # Die Summe der Punkte in allen Spalten in y - Richtung zusammenzählen.
 
-# training set with letters
+        c = np.size(np.where(a > 0)) # Die Anzahl Reihen zusammenzählen, in denen a > 0
+        d = np.size(np.where(b > 0)) # Die Anzahl Spalten zusammenzählen in denen b > 0
+
+        if c > d: # Je nachdem ob c oder d grösser ist, entspricht dies der minimaler möglicher Anzahl Punkte.
+            print(z, c)
+            summe= summe+c
+        else:
+            print(z, d)
+            summe = summe + d
+        return (a, b, c, d,summe)
+
+    print('Das Optimum besteht aus',summe,'Punkten.')
+
+
+
+
+
+
+
+
+def CalcMinCube (cube): # calculating the minimal number of points necessary to display this model
+    sz = cube.shape
+    optimum = np.zeros(sz)
+    summeebene = 0
+    for z in range(sz[2]): # für jede Ebene
+        a = np.sum(cube[:,:,z],0) # Die Summe der Punkte in allen Reihen in x - Richtung zusammenzählen.
+        b = np.sum(cube[:,:,z],1) # Die Summe der Punkte in allen Spalten in y - Richtung zusammenzählen.
+
+        c = np.size(np.where(a > 0)) # Die Anzahl Reihen zusammenzählen, in denen a > 0
+        d = np.size(np.where(b > 0)) # Die Anzahl Spalten zusammenzählen in denen b > 0
+
+        if c > d: # Je nachdem ob c oder d grösser ist, entspricht dies der minimaler möglicher Anzahl Punkte.
+            bb = np.zeros(b.shape)
+            for x in range(sz[0]):
+
+                if a[x] > 0: # a an der Stelle x
+
+                    pointSetFlag = False
+
+                    for y in range(sz[1]):
+
+                        if (cube[x,y,z]>0) and (bb[y] == 0):
+                            optimum[x, y, z] = 1
+                            bb[y] = +1
+                            pointSetFlag = True
+                            break
+                    if pointSetFlag == False:
+                        for y in range(sz[1]):
+                            if (cube[x, y, z] > 0) and (pointSetFlag == False):
+                                optimum[x, y, z] = 1
+                                bb[y] = +1
+                                pointSetFlag = True
+
+
+        else: # man könnte den Würfel hier auch 90 Grad rotieren und das Selbe wie oben machen.
+            aa = np.zeros(a.shape)
+            for y in range(sz[1]):
+                if b[y]>0:
+                    pointSetFlag = False
+
+                    for x in range(sz[0]):
+
+                        if (cube[x, y, z] > 0) and (aa[x] == 0):
+                            optimum[x, y, z] = 1
+                            aa[x] = +1
+                            pointSetFlag = True
+                            break
+                    if pointSetFlag == False:
+                        for x in range(sz[0]):
+                            if (cube[x, y, z] > 0) and (pointSetFlag == False):
+                                optimum[x, y, z] = 1
+                                aa[x] = +1
+                                pointSetFlag = True
+
+
+                    #if sum == summe :
+                       # continue
+                    #else:
+                        #print(z,'Ebene hat eine falsche Anzahl Punkte.')
+
+
+    return(optimum)
+
+
+ # training set with letters
 mnist = MNIST()
 # mnist.train_set.images  (60000, 784) (60000 images, 784 values for the 28*28 grid; arranged as array)
 # mnist.train_set.labels  (60000, 10) (60000 images, 10 different possible labels -one hot encoded (for deep learning))
@@ -77,13 +178,15 @@ cube = vol1 * vol2  # multiplying the two cubes of the two images
 cubegraph = cube > 0  # print only the points where there is a number > 1 (because 0 = no color, 1= black )
 cube[cubegraph] = 1.  # converting all values that are true in cubegraph to a 1.
 
+#calculatemin(cube)
 
 
-cube1, cube2, cube3, cube4 = optimizeAllSides(cube) # Die 4 verschiedenen Cubes herausgeben.
+#cube1, cube2, cube3, cube4 = optimizeAllSides(cube) # Die 4 verschiedenen Cubes herausgeben.
 
 
 #cube = perlenoptimieren(cube)
-cubegraph = cube > 0  # print only the points where there is a number > 1 (because 0 = no color, 1= black )
+optimum = CalcMinCube(cube)
+cubegraph = optimum > 0  # print only the points where there is a number > 1 (because 0 = no color, 1= black )
 
 
 anzahlperlen = np.size(np.where(cubegraph == True))  # count the printed points
@@ -110,17 +213,6 @@ ax.set_zlabel('Z')
 
 # Show the plot
 plt.show()
-
-
-# definition to make a construction plan for my model with pearls
-def printBauplan(cubegraph):
-    g = np.arange(cubegraph.shape[2])  # make an array the length of the z axes
-
-    for x in range(cubegraph.shape[0]):  # check all combinations of x and y coordinates in cubegraph
-        for y in range(cubegraph.shape[1]):
-            v = g[cubegraph[x, y,:]]  # g assigns a number from the array to each true value on the z axes (v) at point x,y
-            if len(v) > 0:  # if there are true values, it prints the z coordinate of these values. (seen from bottom to top)
-                print(x, y, v)
 
 
 printBauplan(cubegraph)
